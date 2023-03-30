5764af7b367c3df2e26566984f8121b65bc99552
deepak7691 <119676343+deepak7691@users.noreply.github.com>
2023-03-31 00:12:19 +0530

    Initial commit

---
+++ src/components/counterContainer.js
@@ -0,0 +1,18 @@
+import { connect } from "react-redux";
+import Counter from "./counter";
+import { increment, decrement, reset } from "../actions";
+
+const mapStateToProps = (state) => {
+    return {
+        counter: state,
+    };
+};
+const mapDispatchToProps = (dispatch) => {
+    return {
+        increment: () => dispatch(increment()),
+        decrement: () => dispatch(decrement()),
+        reset: () => dispatch(reset()),
+    };
+};
+
+// add code here
